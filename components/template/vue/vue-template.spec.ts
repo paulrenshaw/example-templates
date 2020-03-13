56177d78bc36b7399d3a31fb900530a8d2dd794a
Paul Renshaw <paul@renshaw.digital>
2020-03-13 04:51:44 +0000

    fix(template/vue): use kebab-case for filenames

--- components/template/vue/vue-template.spec.ts
+++ components/template/vue/vue-template.spec.ts
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
 import generateVueDefaultCode from './vue-template';
-import vueDefaultCodeSnapshot from './snapshots/vueDefaultCode';
+import vueDefaultCodeSnapshot from './snapshots/vue-default-code';
 
 it('should generate vue default code', () => {
 	const vueDefaultCode = generateVueDefaultCode({ npmId: '@bit/bit.javascript.raw.code-generator' });
